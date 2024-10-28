b6b00e7226b6d31c55e72e24ce745b9120b203e4
Miyan0001 <mochamedandryan@gmail.com>
2024-10-28 15:43:07 +0700

    Update

--- api/index.py
+++ api/index.py
@@ -262,5 +262,5 @@ def process_gemini():
         # return jsonify({'error': f'Unexpected error: {str(e)}'}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host=0.0.0.0)
+# if __name__ == '__main__':
+    # app.run(debug=True, host=0.0.0.0)
