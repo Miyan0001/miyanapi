7aa76804798e2040b4617e2869d3299efff1a172
Miyan0001 <mochamedandryan@gmail.com>
2024-11-11 18:36:03 +0700

    Update

--- api/app.py
+++ api/app.py
@@ -6,7 +6,9 @@ SITE_NAME = 'https://5000-miyan0001-miyan-4n62v1g8a3d.ws-us116.gitpod.io'
 
 @app.route('/<path:path>',methods=['GET'])
 def proxy(path):
-        resp = requests.get(f'{SITE_NAME}{path}')
+    if request.method=='GET':
+        resp = requests.get(f'{SITE_NAME}/{path}')
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in     resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
+    return resp.text
