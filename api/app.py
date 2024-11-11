74405dcf8eaa54fd6d3092b0fc22ba2750406a8f
Miyan0001 <mochamedandryan@gmail.com>
2024-11-11 18:31:34 +0700

    Update

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask,request,redirect,Response
 import requests
 
 app = Flask(__name__)
-SITE_NAME = 'https://crucial-renewing-sparrow.ngrok-free.app'
+SITE_NAME = 'https://5000-miyan0001-miyan-4n62v1g8a3d.ws-us116.gitpod.io'
 
 @app.route('/<path:path>',methods=['GET'])
 def proxy(path):
