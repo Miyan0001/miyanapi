f2c193f6c207482cafe8cbc6d4a96bde9fcb3a83
Miyan0001 <mochamedandryan@gmail.com>
2024-11-11 18:37:15 +0700

    Update

--- api/app.py
+++ api/app.py
@@ -4,11 +4,8 @@ import requests
 app = Flask(__name__)
 SITE_NAME = 'https://5000-miyan0001-miyan-4n62v1g8a3d.ws-us116.gitpod.io'
 
-@app.route('/<path:path>',methods=['GET'])
+@app.route('/',methods=['GET'])
 def proxy(path):
     if request.method=='GET':
-        resp = requests.get(f'{SITE_NAME}/{path}')
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in     resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
+        resp = requests.get(f'{SITE_NAME}/')
     return resp.text
